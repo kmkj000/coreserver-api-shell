259d9893a2c9ce7e01a7023a71045f2347e8a6af
pine64 <kd1191681@st.kobedenshi.ac.jp>
2018-06-21 18:56:05 +0000

    initialize commit

---
+++ coreserver_allow_API.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. ./enviroment.sh
+curl -Ss -o /dev/null https://api.coreserver.jp/v1/tool/ssh_ip_allow -X POST \
+    -d "api_secret_key=${API_KEY}&server_name=${SERVER_NAME}&account=${ACCOUNT}&param[addr]=${IP_ADDR}"
---
+++ enviroment.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#env-----------
+ACCOUNT='自分のアカウント'
+SERVER_NAME='使用サーバ'
+API_KEY='APIキーを取得してコピペ'
+IP_ADDR=`echo $(curl -Ss inet-ip.info)`
