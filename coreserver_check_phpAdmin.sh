13a250126beb551cca11ea6e3d72127ac0a58cad
pine64 <kd1191681@st.kobedenshi.ac.jp>
2018-06-21 19:01:49 +0000

    fix file_name

--- coreserver_check_phpAdmin.sh
+++ coreserver_check_phpAdmin.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-. ./enviroment.sh
+. ./enviroment
 curl -O -s  https://api.coreserver.jp/v1/cms/phpadmin_check -X POST -d "api_secret_key=${API_KEY}&server_name=${SERVER_NAME}&account=${ACCOUNT}"
